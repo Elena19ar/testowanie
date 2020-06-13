2c5be502f53207e482daac8a35ce1daae4164b8d
Elena19ar <45660258+Elena19ar@users.noreply.github.com>
2020-06-13 21:16:38 +0200

    repko

---
+++ WebApi/WebApi/Controllers/JuiceController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApi.Entity;
+using WebApi.Interface;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JuiceController : ControllerBase
+    {
+        private readonly ILogger<JuiceController> _logger;
+        private readonly IJuiceBuilder _juiceBuilder;
+
+        public JuiceController(ILogger<JuiceController> logger, IJuiceBuilder juiceBuilder)
+        {
+            _logger = logger;
+            _juiceBuilder = juiceBuilder;
+        }
+
+        [HttpGet]
+        public Juice Get(int NumberOfPeople, int NumberOfPeopleNotInterest)
+        {
+            var order = new Order { NumberOfPeople = NumberOfPeople, NumberOfPeopleNotInterest = NumberOfPeopleNotInterest };
+            _juiceBuilder.CreateNewJuice(order);
+            return _juiceBuilder.GetJuice();
+        }
+    }
+}
---
+++ WebApi/WebApi/Entity/Orange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Interface;
+
+namespace WebApi.Entity
+{
+    public class Orange
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+    }
+}
